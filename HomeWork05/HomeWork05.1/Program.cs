8f6f3038042513d9ba5d17ea00608e79bb7807e7
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-16 12:06:55 +0300

    Resolved task05.1

--- HomeWork05/HomeWork05.1/Program.cs
+++ HomeWork05/HomeWork05.1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter first number: ");
+bool isInt1 = int.TryParse(Console.ReadLine(), out int firstNumber);
+Console.Write("Enter second number: ");
+bool isInt2 = int.TryParse(Console.ReadLine(), out int secondNumber);
+
+while (!(isInt1 && isInt2))
+{
+	Console.WriteLine("\nYou entered wrong data");
+
+	Console.Write("\nEnter first number: ");
+	isInt1 = int.TryParse(Console.ReadLine(), out firstNumber);
+
+	Console.Write("Enter second number: ");
+	isInt2 = int.TryParse(Console.ReadLine(), out secondNumber);
+}
+
+try
+{
+	double result = (double)firstNumber / secondNumber;
+
+	Console.WriteLine($"\nResult: {firstNumber} / {secondNumber} = {result}");
+}
+catch (DivideByZeroException ex)
+{
+	Console.WriteLine(ex.Message);
+}
