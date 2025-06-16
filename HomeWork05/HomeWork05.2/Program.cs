676583a86d3fddce52e6dd9a4082cd4d04f7898d
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-06-16 12:45:22 +0300

    Resolved task05.2

--- HomeWork05/HomeWork05.2/Program.cs
+++ HomeWork05/HomeWork05.2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter number: ");
+bool isInt = int.TryParse(Console.ReadLine(), out int userInput);
+
+try
+{
+	if (!isInt)
+	{
+		throw new Exception("You entered invalid data");
+	}
+	
+	Console.WriteLine($"Entered number: {userInput}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
